fb96ca348aacfb47a1d699c4228f9896301b1f49
CodeLewisYates <lewis_yates@ymail.com>
2021-04-02 15:55:15 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,141 @@
+import * as THREE from "three";
+import fragment from "./shader/fragment.glsl";
+import vertex from "./shader/vertex.glsl";
+let OrbitControls = require("three-orbit-controls")(THREE);
+
+export default class Sketch {
+  constructor(options) {
+    this.scene = new THREE.Scene();
+
+    this.container = options.dom;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    console.log(this.container);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 0);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
+
+    // var frustumSize = 10;
+    // var aspect = window.innerWidth / window.innerHeight;
+    // this.camera = new THREE.OrthographicCamera(
+    //   (frustumSize * aspect) / -2,
+    //   (frustumSize * aspect) / 2,
+    //   frustumSize / 2,
+    //   frustumSize / -2,
+    //   -1000,
+    //   1000
+    // );
+    this.camera.position.set(0, 0, 1);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.time = 0;
+
+    this.isPlaying = true;
+
+    this.addObjects();
+    this.resize();
+    this.render();
+    this.setupResize();
+    this.mouseEvents();
+    // this.settings();
+  }
+
+  mouseEvents() {
+    this.mouse = new THREE.Vector2();
+    let that = this;
+    function onMouseMove(event) {
+      // calculate mouse position in normalized device coordinates
+      // (-1 to +1) for both components
+
+      that.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      that.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      that.material.uniforms.mouse.value = that.mouse;
+    }
+
+    window.addEventListener("mousemove", onMouseMove, false);
+  }
+
+  settings() {
+    let that = this;
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, "progress", 0, 1, 0.01);
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    let that = this;
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable",
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { type: "f", value: 0 },
+        mouse: { type: "v2", value: new THREE.Vector2(0, 0) },
+        resolution: { type: "v4", value: new THREE.Vector4() },
+        uvRate1: {
+          value: new THREE.Vector2(1, 1),
+        },
+      },
+      // wireframe: true,
+      //transparent: true,
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    });
+
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    this.test = new THREE.CircleGeometry(0.7, 64);
+
+    this.plane = new THREE.Mesh(this.test, this.material); // geometry isntead of test
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (!this.isPlaying) {
+      this.render();
+      this.isPlaying = true;
+    }
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+new Sketch({
+  dom: document.getElementById("container"),
+});
